3bd4ce35258a7449c0d2b7eb29fd6a7a215a8255
nickathanasop <38087410+nickathanasop@users.noreply.github.com>
2020-01-01 14:21:02 +0200

    Change

--- PetPairsStarter/src/model/Human.java
+++ PetPairsStarter/src/model/Human.java
@@ -68,8 +68,13 @@ public class Human {
 
     //EFFECTS: returns the number of pets belonging to species
     public int numPetsOfSpecies(String species) {
-        //TODO 6
-        return 0;
+        int numOfPets = 0;
+        for (Pet p : pets) {
+            if (p.getSpecies().equals(species)) {
+                numOfPets++;
+            }
+        }
+        return numOfPets;
     }
 
 }
