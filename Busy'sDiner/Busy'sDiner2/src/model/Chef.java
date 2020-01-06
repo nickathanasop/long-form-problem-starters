0a6a87cba4c44fbe63ab8a03042ab9590a4c0922
nickathanasop <38087410+nickathanasop@users.noreply.github.com>
2020-01-06 23:28:46 +0200

    Change

--- Busy'sDiner/Busy'sDiner2/src/model/Chef.java
+++ Busy'sDiner/Busy'sDiner2/src/model/Chef.java
@@ -28,12 +28,16 @@ public class Chef {
 
     //EFFECTS: prints out the ingredients being prepared
     private void prepareIngredients() {
-        System.out.println(PREFIX + "Slicing tomatoes... Shredding lettuce...");
+        System.out.print(PREFIX + "Preparing ingredients: ");
+        for (String ingredient : this.order.getIngredients()) {
+            System.out.print(ingredient + "... ");
+        }
+        System.out.print("Trendy!\n");
     }
 
     //EFFECTS: prints out the recipe being followed
     private void followRecipe() {
-        System.out.println(PREFIX + "Stacking meat... Placing veggies.... ");
+        System.out.println(PREFIX + this.order.getRecipe());
     }
 
     //EFFECTS: prints out a message about cooking food
